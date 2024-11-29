0f0b36f86d3fc89e374fb46e5348237822ba04ba
Taro Yamaguchi <work@yamaguchi-taro.com>
2024-11-30 00:43:54 +0900

    type=numberの数値制御処理を追加

--- jimdo/sample_request.js
+++ jimdo/sample_request.js
@@ -104,6 +104,15 @@ $(document).ready(function(){
         }, 500);
     });
 
+    // type=numberの数値制御
+    $('input[type="number"]').focusout(function() {
+        if(typeof $(this).attr('min') !== "undefined" && parseInt($(this).val()) < parseInt($(this).attr('min')))
+            $(this).val($(this).attr('min'));
+        else if(typeof $(this).attr('max') !== "undefined" && parseInt($(this).val()) > parseInt($(this).attr('max')))
+            $(this).val($(this).attr('max'));
+        else if(typeof $(this).attr('min') !== "undefined" && $(this).val() === '')
+            $(this).val($(this).attr('min'));
+    });    
 });
 function toHalfWidth(str) {
     // 全角英数字を半角に変換
